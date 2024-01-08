f52b0b00f039ca012937489464fbf684f559e73b
zYuna646 <anggarangga646@gmail.com>
2024-01-08 18:54:03 +0800

    add component inputForm

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import PrimaryButton from './src/component/PrimaryButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <PrimaryButton title="Edit Profil"/>
     </View>
   );
 }
---
+++ src/component/Button/LogOutButton.js
@@ -0,0 +1,30 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+
+export default function LogOutButton({onPress, title}) {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.button}>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>{title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: 'red',
+        padding: 12,
+        borderRadius: 5,
+        width: '90%',
+      },
+      buttonContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+      },
+      buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        fontFamily: "Poppins"
+      },
+});
