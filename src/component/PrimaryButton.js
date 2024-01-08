351e048f24835f0123045fba4991076b27ed7d5e
zYuna646 <anggarangga646@gmail.com>
2024-01-08 18:41:24 +0800

    add component primaryButton

--- src/component/PrimaryButton.js
+++ src/component/PrimaryButton.js
@@ -1,12 +1,14 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
 
 export default function PrimaryButton() {
   return (
-    <View>
-      <Text>PrimaryButton</Text>
-    </View>
-  )
+    <TouchableOpacity onPress={onPress} style={styles.button}>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>{title}</Text>
+      </View>
+    </TouchableOpacity>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
