f88a912f26d80bbeed135ef1667ccb90fcbf8a0e
zYuna646 <anggarangga646@gmail.com>
2024-01-08 18:40:32 +0800

    add base app

---
+++ src/component/PrimaryButton.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React from 'react'
+
+export default function PrimaryButton() {
+  return (
+    <View>
+      <Text>PrimaryButton</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
