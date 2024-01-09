9aa8878cd42779ce4577d77c20edc7fa76188a7e
zYuna646 <anggarangga646@gmail.com>
2024-01-09 17:22:52 +0800

    add component TextButton

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import PasswordForm from './src/component/Form/PasswordForm';
 import GoogleButton from './src/component/Button/GoogleButton';
+import TextButton from './src/component/Button/TextButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <GoogleButton title="Masuk Menggunakan Google" />
+      <TextButton title="Lupa Password?"/>
     </View>
   );
 }
---
+++ src/component/Button/TextButton.js
@@ -0,0 +1,17 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+
+export default function TextButton({ onPress, title }) {
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonText: {
+    fontSize: 12,
+    color: "#00A3FF",
+  },
+});
